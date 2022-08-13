895d724d1a80be953d970ef6cfa7d1b08238465f
Arnav <arnchkv@gmail.com>
2022-08-13 17:31:07 +0530

    json send from route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'lorem ipsum'
+            ]
+        ]
+    ]);
 });
 
 Route::get('/hello', function () {
